9d90af2dd45a1f175a568b047b96f5ff6efae44f
mojun <mojun@longmaster.com.cn>
2022-03-11 11:42:33 +0800

    修改lua

--- default-route/handler.lua
+++ default-route/handler.lua
@@ -3,7 +3,8 @@ local DefaultRouteHandler = {}
 DefaultRouteHandler.PRIORITY = 1
 DefaultRouteHandler.VERSION = "0.1.0"
 function DefaultRouteHandler:response(conf)
-    kong.response.exit(conf.status_code, conf.tip_message)
+    local content_type = conf.content_type
+    kong.response.exit(conf.status_code, conf.tip_message, {["Content-Type"] = content_type})
 end
 
 return DefaultRouteHandler
--- default-route/schema.lua
+++ default-route/schema.lua
@@ -8,7 +8,8 @@ return {
                 type = "record",
                 fields = {
                     { status_code = { type = "integer", required = false, default = 200 } },
-                    { tip_message = { type = "string", required = false, default = "backend not found" } }
+                    { tip_message = { type = "string", required = false, default = "backend not found" } },
+                    { content_type = { type = "string", required = false, default = "text/html;charset=UTF-8" } }
                 },
             },
         },
